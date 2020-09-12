9f17a28c83abd2afbfed2f8508e42eeb25f041bc
v.priyanka <v.priyanka@blockon.tech>
2020-09-12 21:33:45 +0530

    dashboard updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalServiceService } from '../global-service.service';
 // import { FormBuilder } from '@angular/forms'
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,18 +11,38 @@ import { GlobalServiceService } from '../global-service.service';
 export class DashboardComponent implements OnInit {
 newresponse : any;
 response : any;
+res: any;
+public postData: any = {};
   constructor(private data: GlobalServiceService,
+    private router: Router,
   	// private formBuilder: FormBuilder,
   	 ) {}
 
 
-allData() {
 
-   var postData = {
+allData(){
+  debugger;
+ let postData = {
         limit : 100,
    }
-  this.data.getAllData(postData).subscribe(data => {
-  	this.response = data;
+  this.data.getAllData(postData).subscribe(res => {
+    // this.newresponse = [];
+    this.response = res;
+
+    if (this.response.statusCode == 200) {
+      this.newresponse = this.response;
+    }
+    
+
+     })
+
+}
+allData2() {
+   let postData = {
+        limit : 100,
+   }
+  this.data.getAllData(postData).subscribe(res => {
+  	this.response = res;
   	 if (this.response.statusCode == 200) {
   	 	this.newresponse = this.response;
   	 	console.log('response');
